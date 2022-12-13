248a072904edc7bca935fb49afc1b1fa1baad62e
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-13 15:33:47 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Booking;
+use App\Models\Passanger;
+use App\Models\Station;
+use App\Models\Trip;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +19,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Station::factory(4)->create();
+        Trip::factory(10)->create()->each(function (Trip $trip) {
+            Booking::factory(1)->create([
+                "trip_from" => $trip->id,
+                "trip_back" => $trip->id !== 1 ? $trip->id - 1 : 4,
+            ])->each(function (Booking $booking) {
+                User::factory(1)->create()->each(function (User $user) use ($booking) {
+                    Passanger::factory(1)->create([
+                        "booking_id" => $booking->id,
+                        "first_name" => $user->first_name,
+                        "last_name" => $user->last_name,
+                        "document_number" => $user->document_number
+                    ]);
+                });
+            });
+        });
     }
 }
