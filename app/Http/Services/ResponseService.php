ea28e8c50b83bed9af66d088e0562ab34460fc09
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-13 15:33:06 +0300

    Create ResponseService.php

---
+++ app/Http/Services/ResponseService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Services;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Response;
+
+class ResponseService
+{
+    public static function success(mixed $data = null, int $code = 200, string $message = "successful"): Response|Application|ResponseFactory
+    {
+        return response([
+            "status" => $code,
+            "success" => true,
+            "message" => $message,
+            "data" => $data,
+            "time" => Carbon::now()
+        ], $code);
+    }
+
+    public static function error(int $code = 422, string $message = "unsuccessful", mixed $data = null): Response|Application|ResponseFactory
+    {
+        return response([
+            "status" => $code,
+            "success" => false,
+            "message" => $message,
+            "data" => $data,
+            "time" => Carbon::now()
+        ], $code);
+    }
+}
