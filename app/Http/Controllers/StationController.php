edd6b6101aebe953d77b700bfa619ecbca52af05
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-13 15:34:10 +0300

    Update StationController.php

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StationSearchRequest;
+use App\Http\Resources\StationResource;
+use App\Http\Services\ResponseService;
+use App\Models\Station;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class StationController extends Controller
 {
-    //
+    public function search(StationSearchRequest $request): Response|Application|ResponseFactory
+    {
+        $searched =  "%" . strtolower($request->query('query')) . "%";
+        $stations = Station::where("city", "like", $searched)->
+        orWhere("station", "like", $searched)->get();
+        return ResponseService::success(StationResource::collection($stations));
+    }
 }
