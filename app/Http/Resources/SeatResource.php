778a449a273b1025e2a6a42b17e260c7d9b83f1e
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-12-13 23:01:04 +0300

    Create SeatResource.php

---
+++ app/Http/Resources/SeatResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SeatResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "occupied_from" => SeatFromResource::collection($this->passengers),
+            "occupied_back" => SeatBackResource::collection($this->passengers)
+        ];
+    }
+}
