c6fc6eedf52d7408d554158655f969398e69f1d9
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-12-13 23:01:40 +0300

    Новые методы для api

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TripSearchRequest;
+use App\Http\Resources\TripResource;
+use App\Models\Station;
+use App\Models\Trip;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TripController extends Controller
 {
-    public function search()
+    public function search(TripSearchRequest $request): Response|Application|ResponseFactory
     {
+        $stationFrom = Station::where("code", $request->from)->first();
+        $stationTo = Station::where("code", $request->to)->first();
+        $tripTo = Trip::where("from_id", $stationFrom->id)->
+        where("to_id", $stationTo->id)->get();
 
+        if ($request->date2) {
+            $tripBack = Trip::where("to_id", $stationFrom->id)->
+            where("from_id", $stationTo->id)->get();
+            return response([
+                "trip_to" => TripResource::collection($tripTo),
+                "trip_back" => TripResource::collection($tripBack)
+            ]);
+        }
+
+        return response(["trip_to" => TripResource::collection($tripTo)]);
     }
 }
