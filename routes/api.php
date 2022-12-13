94cf6d403789436012db2145770d1d5eb341cb5e
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-12-13 23:00:58 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\StationController;
+use App\Http\Controllers\TripController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,9 +22,19 @@ use Illuminate\Support\Facades\Route;
 Route::post("login", [UserController::class, "login"]);
 Route::post("register", [UserController::class, "store"]);
 Route::get("station", [StationController::class, "search"]);
-Route::get("trip", [StationController::class, "search"]);
+Route::get("trip", [TripController::class, "search"]);
+Route::prefix("/booking")->group(function () {
+    Route::post("", [BookingController::class, "store"]);
+    Route::get("/{code}", [BookingController::class, "index"]);
+    Route::get("/{code}/seat", [BookingController::class, "indexSeat"]);
+    Route::patch("/{code}/seat", [BookingController::class, "update"]);
+});
 
-Route::middleware("auth:api")->group(function (){
+Route::middleware("auth:api")->group(function () {
+    Route::prefix("/user")->group(function (){
+        Route::get("", [UserController::class, "index"]);
+        Route::get("/booking", [UserController::class, "indexBookings"]);
+    });
 
 });
 
