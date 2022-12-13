64ca6ad0d6385071383613070e080d7dc95819e6
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-12-13 23:02:30 +0300

    Явные связи

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,18 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Booking extends Model
 {
     use HasFactory;
     protected $guarded = [];
 
-    public function tripFrom()
+    public function tripFrom(): BelongsTo
     {
         return $this->belongsTo(Trip::class, "trip_from");
     }
-    public function tripTo()
+    public function tripBack(): BelongsTo
     {
-        return $this->belongsTo(Trip::class, "trip_to");
+        return $this->belongsTo(Trip::class, "trip_back");
+    }
+
+    public function trips()
+    {
+        return $this->belongsTo(Trip::class);
+    }
+    public function passengers(): HasMany
+    {
+        return $this->hasMany(Passanger::class);
     }
 }
