d88a9eb6a00c7a651ffca187cc82af2db58f9b5c
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-13 15:33:26 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\StationController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::post("login", [UserController::class, "login"]);
+Route::post("register", [UserController::class, "store"]);
+Route::get("station", [StationController::class, "search"]);
+Route::get("trip", [StationController::class, "search"]);
 
 Route::middleware("auth:api")->group(function (){
 
